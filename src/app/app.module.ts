a2ad5020c18907343644c61bf99b4314799f7ce9
Oleg <olegbespalko12@gmail.com>
2021-03-25 16:10:40 +0200

    feat: - created notes-page.component - created create-note-page.component - created edit-note-page.component - created single-note-page.component - created settings-page.component - added main routes to app-routing.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NotesPageComponent } from './pages/notes-page/notes-page.component';
+import { CreateNotePageComponent } from './pages/create-note-page/create-note-page.component';
+import { EditNotePageComponent } from './pages/edit-note-page/edit-note-page.component';
+import { SingleNotePageComponent } from './pages/single-note-page/single-note-page.component';
+import { SettingsPageComponent } from './pages/settings-page/settings-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NotesPageComponent,
+    CreateNotePageComponent,
+    EditNotePageComponent,
+    SingleNotePageComponent,
+    SettingsPageComponent
   ],
   imports: [
     BrowserModule,
